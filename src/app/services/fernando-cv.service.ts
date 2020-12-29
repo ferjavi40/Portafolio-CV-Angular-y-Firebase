e7905c465dd6c9887a8679ecdea6290a35dc25d8
Fernando Ramirez <ferjaviramirez@gmail.com>
2020-12-29 21:48:43 +0100

    seecion resumen terminada, funcionando

--- src/app/services/fernando-cv.service.ts
+++ src/app/services/fernando-cv.service.ts
@@ -5,19 +5,23 @@ import {HttpClient} from '@angular/common/http'
   providedIn: 'root'
 })
 export class FernandoCVService {
-  profesional: any[] = [];
-  educacion: any[] = [];
+  profesional: any [] = [];
+  educacion: any [] = [];
+  experiencia: any [] = [];
+  testimonio: any[] = [];
 
   constructor( private http:HttpClient) { 
     this.CargarProfesional();
     this.CargarEducacion();
+    this.CargarExperiencia();
+    this.CargarTestimonio();
   }
 
   private CargarProfesional(){
     this.http.get('https://angularcv-39be2-default-rtdb.firebaseio.com/Profesional.json')
     .subscribe((resp: any[])=>{
       this.profesional= resp;
-      console.log(resp);
+      // console.log(resp);
     })
   }
 
@@ -25,6 +29,22 @@ export class FernandoCVService {
     this.http.get('https://angularcv-39be2-default-rtdb.firebaseio.com/educacion.json')
     .subscribe((resp: any[])=>{
       this.educacion= resp;
+      // console.log(resp);
+    })
+  }
+
+  private CargarExperiencia(){
+    this.http.get('https://angularcv-39be2-default-rtdb.firebaseio.com/Experiencia.json')
+    .subscribe((resp: any[])=>{
+      this.experiencia= resp;
+      // console.log(resp);
+    })
+  }
+
+  private CargarTestimonio(){
+    this.http.get('https://angularcv-39be2-default-rtdb.firebaseio.com/testimonio.json')
+    .subscribe((resp: any[])=>{
+      this.testimonio= resp;
       console.log(resp);
     })
   }
